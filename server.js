ca80ed06d3081e9c0cb2ee87399a9498f51a8827
dukewellington61 <dukewellington61@googlemail.com>
2021-01-25 21:40:39 +0100

    deploy3
    details:
    code is server.js added for:
    Serve static assets in production
    Set static folder

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/positions", require("./routes/api/positions"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
