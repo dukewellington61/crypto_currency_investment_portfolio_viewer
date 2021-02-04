4771663d6e3c94f812ed7b765fbb460e792548fa
dukewellington61 <dukewellington61@googlemail.com>
2021-02-04 17:40:06 +0100

    alert

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Alert = ({ alert, removeAlert }) => {
   return (
-    <div id="alert" className={`alert alert-${alert.type}`} role="alert">
+    <div
+      id="alert"
+      className={`alert alert-${alert.type}`}
+      role="alert"
+      style={{ display: Object.keys(alert).length > 0 ? "block" : "none" }}
+    >
       {/* <div id="remove_alert" onClick={removeAlert}>
         x
       </div> */}
